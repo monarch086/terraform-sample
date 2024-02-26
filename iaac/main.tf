fd481c621299dc98db54196b08c1de65a9868448
Oleksandr Barsuk <sbarsuk88@gmail.com>
2024-02-26 21:33:26 +0000

    Add modules example

--- iaac/main.tf
+++ iaac/main.tf
@@ -24,6 +24,35 @@ resource "aws_instance" "app_server" {
   vpc_security_group_ids = [data.aws_security_group.selected.id]
 
   tags = {
-    Name = "ExampleInstance"
+    Name = "barsuk_instance"
+  }
+}
+
+# module "s3_bucket" {
+#   source  = "terraform-aws-modules/s3-bucket/aws"
+#   version = "~> 3.0"
+
+#   bucket = "barsuk-module-bucket"
+#   acl    = "private"
+
+#   tags = {
+#     Name        = "Barsuk bucket"
+#     Environment = "Dev"
+#   }
+# }
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name           = "barsuk-module-instance"
+
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = local.key_pair
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
+
+  tags = {
+    Name = "barsuk_module_instance"
   }
 }
